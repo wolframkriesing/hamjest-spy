edba06723c17635a595ca5afc60e3a9e6e5b84e6
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-12-22 13:45:28 +0100

    Some more examples for how to use the matchers.

--- src/matcher.spec.js
+++ src/matcher.spec.js
@@ -1,10 +1,11 @@
-import { assertThat, not } from 'hamjest';
+import { assertThat, not, greaterThan } from 'hamjest';
 import {
   buildFunctionSpy,
-  wasCalled, wasNotCalled, firstCallArgsWere,
+  wasCalled, wasNotCalled, callCountWas,
+  firstCallArgsWere, lastCallArgsWere,
 } from './index';
 
-describe('Matchers', () => {
+describe('Matchers:', () => {
   describe('`wasCalled()`', () => {
     it('fires when spied-on function got called', () => {
       const f = buildFunctionSpy();
@@ -46,6 +47,53 @@ describe('Matchers', () => {
       const f = buildFunctionSpy();
       f();
       assertThat(f, not(firstCallArgsWere(['expected arg'])));
+    });
+  });
+
+  describe('`lastCallArgsWere()`', () => {
+    it('passes when last call`s args match', () => {
+      const args = [1, 'two'];
+      const f = buildFunctionSpy();
+
+      f(args[0], args[1]);
+
+      assertThat(f, lastCallArgsWere(args));
+    });
+    it('passes even when other args were used before', () => {
+      const args = [1, 'two'];
+      const f = buildFunctionSpy();
+
+      f();                  // first call
+      f(args[0], args[1]);  // last call
+
+      assertThat(f, lastCallArgsWere(args));
+    });
+    it('bails when args don`t match', () => {
+      const args = [];
+      const f = buildFunctionSpy();
+
+      f(1, 'two');
+
+      assertThat(f, not(lastCallArgsWere(args)));
+    });
+  });
+
+  describe('`callCountWas()`', () => {
+    it('passes when spy got called once', () => {
+      const f = buildFunctionSpy();
+
+      f();
+
+      assertThat(f, callCountWas(1));
+    });
+    it('passes for multiple calls', () => {
+      const f = buildFunctionSpy();
+
+      f();
+      f();
+      f();
+
+      assertThat(f, callCountWas(3));
     });
   });
 });
