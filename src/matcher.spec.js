f421c7c54fd0c6c01a1b1459583562fcc3e46277
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-12-22 13:38:21 +0100

    Tests for `firstCallArgsWere()`.

--- src/matcher.spec.js
+++ src/matcher.spec.js
@@ -1,7 +1,7 @@
 import { assertThat, not } from 'hamjest';
 import {
   buildFunctionSpy,
-  wasCalled, wasNotCalled,
+  wasCalled, wasNotCalled, firstCallArgsWere,
 } from './index';
 
 describe('Matchers', () => {
@@ -26,6 +26,26 @@ describe('Matchers', () => {
       const f = buildFunctionSpy();
       f();
       assertThat(f, not(wasNotCalled()));
+    });
+  });
+
+  describe('`firstCallArgsWere()`', () => {
+    it('passes when spy was called with expected args', () => {
+      const firstArgument = '1';
+      const f = buildFunctionSpy();
+      f(firstArgument);
+      assertThat(f, firstCallArgsWere([firstArgument]));
+    });
+    it('fails when spy was called with more args', () => {
+      const firstArgument = '1';
+      const f = buildFunctionSpy();
+      f(firstArgument, 'unexpected 2nd arg');
+      assertThat(f, not(firstCallArgsWere([firstArgument])));
+    });
+    it('fails when spy was called without args', () => {
+      const f = buildFunctionSpy();
+      f();
+      assertThat(f, not(firstCallArgsWere(['expected arg'])));
     });
   });
 });
